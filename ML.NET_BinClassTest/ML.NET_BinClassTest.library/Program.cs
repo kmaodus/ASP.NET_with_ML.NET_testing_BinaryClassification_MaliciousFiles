5adaa6c59f744b874fff2f895df81f3311c146cc
Kristijan Maoduš <kmaodus@foi.hr>
2021-08-28 00:24:41 +0200

    Added projects to the solution

---
+++ ML.NET_BinClassTest/ML.NET_BinClassTest.library/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML.NET_BinClassTest.library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ML.NET_BinClassTest/ML.NET_BinClassTest.trainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML.NET_BinClassTest.trainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
